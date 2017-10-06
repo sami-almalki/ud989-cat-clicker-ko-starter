8a0b396337fffd179a50d8bda931c201ab0b13e3
Sami Almalki <samifx@gmail.com>
2017-10-06 19:44:49 +0300

    Added more cats!

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
-var ViewModel = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+var initialCats = [
+  {
+    'clickCount': 0,
+    'name': 'Catty',
+    'imgSrc': 'img/22252709_010df3379e_z.jpg',
+    'nickName': [ {name: 'catty'}, {name: 'cute cat'}]
+  },
+  {
+    'clickCount': 0,
+    'name': 'Kitty',
+    'imgSrc': 'img/434164568_fea0ad4013_z.jpg',
+    'nickName': [ {name: 'kitty'}, {name: 'cute kitty'}]
+  },
+  {
+    'clickCount': 0,
+    'name': 'Kitten',
+    'imgSrc': 'img/1413379559_412a540d29_z.jpg',
+    'nickName': [ {name: 'kitten'}, {name: 'cute kitten'}]
+  },
+  {
+    'clickCount': 0,
+    'name': 'Cutie',
+    'imgSrc': 'img/4154543904_6e2428c421_z.jpg',
+    'nickName': [ {name: 'cutie'}, {name: 'cute cutie'}]
+  }
+]
 
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.nickName = ko.observableArray(data.nickName);
 
   this.level = ko.computed(function() {
     if (this.clickCount() < 10) {
@@ -16,6 +40,26 @@ var ViewModel = function() {
       return 'teen';
     }
   }, this);
+
+}
+
+var ViewModel = function() {
+  self = this;
+
+  this.catList = ko.observableArray([]);
+  initialCats.forEach(function (catItem) {
+    self.catList.push( new Cat(catItem) );
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
+
+  this.incrementCounter = function() {
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
+  this.setCat = function(clickedCat) {
+    self.currentCat(clickedCat);
+  };
 }
 
 ko.applyBindings(new ViewModel);
